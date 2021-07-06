95dbac45b1d5976cc49597ad1dd0073e39c9f755
Pushkar Kothavade <pushkarbk@gmail.com>
2021-07-06 14:18:29 +0530

    Create server.go

---
+++ server.go
@@ -0,0 +1,11 @@
+func main() {
+    http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hello!")
+    })
+
+
+    fmt.Printf("Starting server at port 80\n")
+    if err := http.ListenAndServe(":80", nil); err != nil {
+        log.Fatal(err)
+    }
+}
